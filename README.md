e9ccee116c7b325b8decacc1dc5a6219f19f7665
Ryan Peach <ryanpeach@me.com>
2023-01-06 16:04:00 -0500

    Got gpt to do stuff

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pollybooks
+
+# Instructions
+
+1. Install Pandoc https://pandoc.org/installing.html
+2. Get an OpenAI API key, save it to .env
+3. Get an AWS IAM key, save it to .env
+
+# Usage
+
+1. Create a new book in the `books` directory. It should be a single text file. The format can be anything.
+
+2. Chop off anything above the first chapter or below the last chapter.
+
---
+++ pollybooks/test_preprocessing.py
@@ -0,0 +1,16 @@
+from pollybooks.preprocessing import split_text_into_n_token_strings
+
+def test_split_text_into_n_token_strings():
+    """ Test this function using a small input text """
+    test_txt = """
+    This is a test. This is only a test. 36
+
+    Why is this a test? Because it's a test. 40
+
+    You cant test this test. You can only test the test. 52
+    """
+
+    # Split the text into chunks of 15 or less tokens
+    chunks = split_text_into_n_token_strings(test_txt, max_tokens=15, n_chars_per_token=4)
+
+    assert len(chunks) == 3
